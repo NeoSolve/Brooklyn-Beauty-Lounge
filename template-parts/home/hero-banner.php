0a3df0e740f2a22c9dfbe57a10989b129f151dc5
artemonypchenko <artem.onypchenko@gmail.com>
2026-02-12 02:05:44 +0200

    ui-kit: add button component

--- template-parts/home/hero-banner.php
+++ template-parts/home/hero-banner.php
@@ -6,7 +6,7 @@
  */
 $hero_title   = get_theme_mod( 'bb_hero_title', __( 'Brooklyn Beauty Lounge', 'brooklyn-beauty' ) );
 $hero_subtitle = get_theme_mod( 'bb_hero_subtitle', __( 'Your place for beauty and relaxation', 'brooklyn-beauty' ) );
-$hero_btn_text = get_theme_mod( 'bb_hero_btn', __( 'Book an appointment', 'brooklyn-beauty' ) );
+$hero_btn_text = get_theme_mod( 'bb_hero_btn', __( 'make an appointment', 'brooklyn-beauty' ) );
 $hero_btn_url  = get_theme_mod( 'bb_hero_btn_url', '#book' );
 $hero_image    = get_theme_mod( 'bb_hero_image', '' );
 $hero_style    = $hero_image ? ' style="background-image: url(' . esc_url( $hero_image ) . ');"' : '';
@@ -19,7 +19,7 @@ $hero_style    = $hero_image ? ' style="background-image: url(' . esc_url( $hero
 			<p class="bb-hero__subtitle"><?php echo esc_html( $hero_subtitle ); ?></p>
 		<?php endif; ?>
 		<?php if ( $hero_btn_text ) : ?>
-			<a href="<?php echo esc_url( $hero_btn_url ); ?>" class="bb-hero__cta"><?php echo esc_html( $hero_btn_text ); ?></a>
+			<a href="<?php echo esc_url( $hero_btn_url ); ?>" class="bb-hero__cta btn btn--medium btn--primary t-button"><?php echo esc_html( $hero_btn_text ); ?></a>
 		<?php endif; ?>
 	</div>
 </section>
